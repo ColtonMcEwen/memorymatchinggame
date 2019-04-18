803d97b701c2741bfe6d0b42a6f277346273491c
Colton McEwen <colton.mcewen@gmail.com>
2019-04-17 19:02:43 -0600

    Updated CORS in server.py

--- server.py
+++ server.py
@@ -213,6 +213,7 @@ class MyRequestHandler(BaseHTTPRequestHandler):
     def do_OPTIONS(self):
         self.load_session()
         self.send_response(200)
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
         self.send_header("Access-Control-Allow-Headers", "Content-type")
         self.end_headers()
